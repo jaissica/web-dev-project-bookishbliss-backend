9ad8bcc7cb092af00d8df68b6c0fba0749c197e7
jaissica <jaissica.hora@gmail.com>
2023-06-24 18:33:22 -0400

    coomit 8

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
 import User from "./model/user.js"
 
 export const authUser = async (req, res, next) => {
+
   if (req.session && req.session['user'] && req.session['user']._id) {
+
     const user = await User.findById(req.session['user']._id);
     if (user)
       return next();
