a4e05fc82307315946780f050cc6de2d108b153f
jaissica <jaissica.hora@gmail.com>
2023-06-25 15:52:32 -0400

    users

--- model/user.js
+++ model/user.js
@@ -1,7 +1,30 @@
-const getUserLikeBook = async (req, res) => {
-  const user = req.params['uid'];
-  const isbn13 = req.params['isbn'];
-  const like = await Likebook.find({user, isbn13})
-  res.json(like);
-}
+import mongoose from "mongoose";
 
+const user = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  
+  password: {
+    type: String,
+    required: true,
+  },
+
+  fullname: String,
+  bio: String,
+  location: String,
+  dob: Date,
+  website: String,
+  email: String,
+  
+  role: {
+    type: String,
+    enum: ["common", "creator", "admin"],
+    default: "common"
+  }
+}, 
+{collection: "user"})
+
+export default mongoose.model("user", user);
