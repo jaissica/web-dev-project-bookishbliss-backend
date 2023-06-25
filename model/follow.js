8dcb96b1512bb2bb335820df2c9e091ae6175f63
jaissica <jaissica.hora@gmail.com>
2023-06-25 15:50:06 -0400

    follows

--- model/follow.js
+++ model/follow.js
@@ -1,5 +1,17 @@
 import mongoose from "mongoose";
 
+const follow = mongoose.Schema({
+  
+  follower: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+
+  followee: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  }
+}, {collection: "follow", timestamps: true})
 
 
 export default mongoose.model("follow", follow);
